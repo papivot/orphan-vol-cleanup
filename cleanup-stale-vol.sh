7e5a51a3f33d784fed44a9ee98b760c01d8a5d8d
Navneet Verma <47264956+papivot@users.noreply.github.com>
2024-08-01 14:41:11 -0400

    Update cleanup-stale-vol.sh

--- cleanup-stale-vol.sh
+++ cleanup-stale-vol.sh
@@ -5,7 +5,6 @@ export GOVC_URL=
 export GOVC_USERNAME=
 export GOVC_PASSWORD=
 export GOVC_INSECURE=true
-export GOVC_DATACENTER=
 
 if ! command -v govc &> /dev/null
 then
@@ -13,6 +12,7 @@ then
 	exit 1
 fi
 
+echo "Status: Initializing script..."
 kubectl get secret -n vmware-system-csi vsphere-config-secret -o json|jq -r '.data."vsphere-cloud-provider.conf"'|base64 -d > /tmp/cns.txt
 if [ -f /tmp/cns.txt ]
 then
@@ -22,19 +22,25 @@ then
 		echo "Error: Unable to get vCenter information from secret vsphere-config-secret. Exiting..."
 		exit 1
 	fi
+        echo "Init: vCenter URL is - ${GOVC_URL}"
+
 	GOVC_USERNAME=$(grep -i user /tmp/cns.txt |awk -F'"' '{print $2}')
 	if [ -z "${GOVC_USERNAME}" ]
 	then
 		echo "Error: Unable to get vCenter username from secret vsphere-config-secret. Exiting..."
 		exit 1
 	fi
-    	dc=$(grep -i datacenters /tmp/cns.txt |awk -F'"' '{print $2}')
-	if [ -z "${dc}" ]
-	then
-		echo "Error: Unable to get Datacenter name from secret vsphere-config-secret. Exiting..."
-		exit 1
-	fi
-	# The password has a lot of random characters. Care needs to be taken to read it. We probably need to find a better solution. 
+     	echo "Init: vCenter Username is - ${GOVC_USERNAME}"
+    
+    	supervisor=$(grep -i supervisor-id /tmp/cns.txt |awk -F'"' '{print $2}')
+    	if [ -z "${supervisor}" ]
+    	then
+        	echo "Error: Unable to get Supervisor ID from secret vsphere-config-secret. Exiting..."
+        	exit 1
+    	fi
+	SUPERVISOR_ID=vSphereSupervisorID-"${supervisor}"
+	echo "Init: Supervisor ID is - ${SUPERVISOR_ID}"
+
 	TEMP_PASSWORD=$(grep -i password /tmp/cns.txt |awk -F' = ' '{print $2}')
 	if [ -z "${TEMP_PASSWORD}" ]
 	then
@@ -43,74 +49,92 @@ then
 	fi
 	GOVC_PASSWORD_0="${TEMP_PASSWORD%\"}"
 	GOVC_PASSWORD_0="${GOVC_PASSWORD_0#\"}"
-	echo "${GOVC_PASSWORD_0}"
+#	echo "${GOVC_PASSWORD_0}"
+
 	IFS=
 	read -r pw <<< "${GOVC_PASSWORD_0}"
-	echo "$pw"
+#	echo "$pw"
 	#Filter out PVs that are created on the Supervisor only
-	GOVC_PASSWORD="$pw" govc session.login
+	#GOVC_PASSWORD="${pw}" govc session.login -verbose=true
+	GOVC_PASSWORD="${pw}"
 	IFS=$' \t\n'
 
-    	GOVC_DATACENTER=$(govc datacenter.info --json|jq -r --arg dc "$dc" '.datacenters[]|select(.self.value == $dc)|.name')
-    	if [ -z "${GOVC_DATACENTER}" ]
-	then
-		echo "Error: Unable to get valid Datacenter name. Exiting..."
-		exit 1
-	fi
-
-	# Get all CNS volumes for the vCenter containing ContainerClusterArray item of type GUEST_CLUSTER. This filters out the PVs created directly on the Supervisor.
-	# Need to check how to filter by storage/cluster???
+	# Get all CNS volumes for the vCenter containing ContainerClusterArray item of type GUEST_CLUSTER. This filters out the PVs created directly on the Supervisor. Only PVs that have been created on guest clusters are returned. 
 	workload_pv=$(govc volume.ls -json | jq -r '[ .volume[]|select(.Metadata.ContainerClusterArray != null and any (.Metadata.ContainerClusterArray[]; .ClusterFlavor == "GUEST_CLUSTER")) ]')
 	if [ -z "${workload_pv}" ]
 	then
-		echo "Status: No Workload Cluster specific persistant volume found. Exiting..."
+		echo "Status: No Workload Cluster persistant volume found. Exiting..."
 		exit 0
 	fi
 
-	# Dumping the above array to individual files.
+	# Dumping the above array of CNS volumes to individual files.
 	echo "${workload_pv}"| jq -cr '.[]| .VolumeId.Id, .' | awk 'NR%2{f=$0".json";next} {print >f;close(f)}'
 
-	for filename in *.json
+    	echo;echo;
+	echo "Status: Checking for Orphan CNS volumes created on workload clusters..."
+    	for filename in *.json
 	do
-		#echo "Status: Now processing file ${filename}."
-		num_cluster_flavor=$(jq -r '.Metadata.ContainerClusterArray|length' "${filename}")
+		echo "Status: Now processing file ${filename}."
 
-		# We may need to filter the volume with only one GUEST_CLUSTER and one WORKLOAD. Only WORKLOAD implies a PV created on a Supervisor Cluster.
-		# Need to check if the value can be more than 2 (probably for RWM??)
-		if [ "$num_cluster_flavor" -eq 2 ]
+		# Check if the CNS volume belongs to the Supervisor with the ID of SUPERVISOR_ID. If not, skip to the next file. If it does, do further processing. 
+		supid=$(jq -r '.Metadata.ContainerCluster.ClusterId' "${filename}")
+		if [ -n "${supid}" ]
+		then
+			if [ "${supid}" != "$SUPERVISOR_ID" ]
+			then
+				echo "Status: The CNS volume in json file does not belong to the Supervisor. Ignoring file..."
+				rm -f "${filename}"
+                		continue
+			fi
+		else
+			echo "Error: Could not capture valid Metadata.ContainerCluster.ClusterId in the json file. Ignoring file..."
+			rm -f "${filename}"
+            		continue
+		fi
+
+       	 	# Get the number of items in .Metadata.ContainerClusterArray. It should not have a count of 1 but greater than 1. 
+		num_cluster_flavor=$(jq -r '.Metadata.ContainerClusterArray|length' "${filename}")
+		if [ "$num_cluster_flavor" -gt 1 ]
 		then
             		# Once we have filtered this, we need to get the .Metadata.ContainerClusterArray[].ClusterID value where the .Metadata.ContainerClusterArray[].ClusterFlavor == "GUEST_CLUSTER".
-			# Get the clusterID of the Guest cluster. 
-            		# This is the TKG cluster ID.
-			clusterid=$(jq -r '.Metadata.ContainerClusterArray[]| select(.ClusterFlavor == "GUEST_CLUSTER")|.ClusterId' "${filename}")
+			# Get the all the clusterID of the Guest cluster.
+            		# These are the TKG cluster IDs.
+			clusterids=$(jq -r '.Metadata.ContainerClusterArray[]| select(.ClusterFlavor == "GUEST_CLUSTER")|.ClusterId' "${filename}")
+#            		echo "$clusterids"
 
-			# Search for the above ClusterID in .Metadata.EntityMetadata[].ClusterID where .Metadata.EntityMetadata[].EntityType = PERSISTENT_VOLUME or .Metadata.EntityMetadata[].EntityType = PERSISTENT_VOLUME_CLAIM. If found, ignore.
-			cluster_found=$(jq -r --arg cid "$clusterid" '.Metadata.EntityMetadata[]|select (.ClusterID == $cid and (.EntityType == "PERSISTENT_VOLUME" or .EntityType == "PERSISTENT_VOLUME_CLAIM"))' "${filename}")
-			if [ "$cluster_found" ]
-			then
-				echo "Status: ${filename} has no orphan volumes."
-				rm -f "${filename}"
-			else
-				echo "Status: ${filename} has an orphaned volume that may need to be removed."
+            		found=0
+            		for clusterid in $clusterids
+            		do
+                		# Search for the above ClusterID in .Metadata.EntityMetadata[].ClusterID where .Metadata.EntityMetadata[].EntityType = PERSISTENT_VOLUME or .Metadata.EntityMetadata[].EntityType = PERSISTENT_VOLUME_CLAIM. If found, ignore.
+                		cluster_found=$(jq -r --arg cid "$clusterid" '.Metadata.EntityMetadata[]|select (.ClusterID == $cid and (.EntityType == "PERSISTENT_VOLUME" or .EntityType == "PERSISTENT_VOLUME_CLAIM"))' "${filename}")
+                		if [ "$cluster_found" ]
+                		then
+                    			echo "Status: ${filename} has no orphan volumes."
+                    			found=1
+                		fi
+            		done
 
-				storagepolicyid=$(jq -r '.StoragePolicyId' "${filename}")
-				pv_name=$(jq -r '.Name' "${filename}")
-				pvc_name=$(jq -r '.Metadata.EntityMetadata[]|select (.EntityType == "PERSISTENT_VOLUME_CLAIM")|.EntityName' "${filename}")
-				namespace=$(jq -r '.Metadata.EntityMetadata[]|select (.EntityType == "PERSISTENT_VOLUME_CLAIM")|.Namespace' "${filename}")
+            		if [ "$found" -eq 0 ]
+            		then        
+                		echo "Status: ${filename} has an orphaned volume that needs to be cleaned."
+                		pv_name=$(jq -r '.Name' "${filename}")
+               	 		storagepolicyid=$(jq -r '.StoragePolicyId' "${filename}")
+                		namespace=$(jq -r '.Metadata.EntityMetadata[]|select (.EntityType == "PERSISTENT_VOLUME_CLAIM")|.Namespace' "${filename}")
+                		pvc_name=$(jq -r '.Metadata.EntityMetadata[]|select (.EntityType == "PERSISTENT_VOLUME_CLAIM")|.EntityName' "${filename}")
 
-				# Before deleting, check if the .StoragePolicyId is in the list of StoragePolicyId of all the StorageClasses of the current Supervisor.
-				spid_found=$(kubectl get storageclass -o json | jq -r --arg spid "$storagepolicyid" '.items[].parameters|select (.storagePolicyID == $spid)')
-				if [ "$spid_found" ]
-				then
-					# If the reclaim policy of the PV is Delete, then delete the corresponding PVC.
+                		# Before deleting, check if the .StoragePolicyId is in the list of StoragePolicyId of all the StorageClasses of the current Supervisor.
+                		spid_found=$(kubectl get storageclass -o json | jq -r --arg spid "$storagepolicyid" '.items[].parameters|select (.storagePolicyID == $spid)')
+                		if [ "$spid_found" ]
+                		then
+                			# If reclaim policy of the PV is Delete, then delete the corrosponding PVC.
                     			reclaimpolicy=$(kubectl get pv "${pv_name}" -o json|jq -r '.spec.persistentVolumeReclaimPolicy')
                     			if [ "$reclaimpolicy" == "Delete" ]
                     			then
-						echo "kubectl delete pvc ${pvc_name} -n ${namespace}"
+                        			echo "kubectl delete pvc ${pvc_name} -n ${namespace}"
                     			else
-                        			echo "${pv_name} has a reclaimpolicy of Retain. Ignoring..."
+                        			echo "Status: ${pv_name} has a reclaimPolicy of Retain. Ignoring..."
                     			fi
-				fi
+                		fi
 			fi
 		fi
 	done
